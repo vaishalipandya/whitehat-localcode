9c11b4e3fa7ec5853cfd642f77f2fab2f4bbdde8
Komal_EIC <komal.sagar@einfochips.com>
2019-04-24 17:26:28 +0530

    localization-label-changes resolved-sorting-issue-in-rating-in-vuln-grid.

--- WhiteHatSec.VSIX/Utility/DropDownItems.cs
+++ WhiteHatSec.VSIX/Utility/DropDownItems.cs
@@ -22,36 +22,5 @@
 
     /// <summary>
     /// </summary>
-    public enum Severity
-    {
-        /// <summary>
-        ///     All.
-        /// </summary>
-        All = 0,
-
-        /// <summary>
-        ///     The critical.
-        /// </summary>
-        Critical = 1,
-
-        /// <summary>
-        ///     The high.
-        /// </summary>
-        High = 2,
-
-        /// <summary>
-        ///     The medium.
-        /// </summary>
-        Medium = 3,
-
-        /// <summary>
-        ///     The low.
-        /// </summary>
-        Low = 4,
-
-        /// <summary>
-        ///     The informational.
-        /// </summary>
-        Note = 5
-    }
+   
 }
